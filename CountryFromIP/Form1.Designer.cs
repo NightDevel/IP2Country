26f0e6f2c98f48aff59bd662ffa05bb41f966be3
TheLightMC <nateseveryns@gmail.com>
2015-08-07 03:24:50 -0500

    Initial commit

---
+++ CountryFromIP/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace CountryFromIP
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region
+
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(502, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.8F);
+            this.label1.Location = new System.Drawing.Point(242, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP Address:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.8F);
+            this.label2.Location = new System.Drawing.Point(252, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 42);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Country:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F);
+            this.button1.Location = new System.Drawing.Point(520, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 34);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Confirm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.8F);
+            this.textBox2.Location = new System.Drawing.Point(195, 182);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(257, 37);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "N/A";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 290);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "IP 2 Country";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private TextBox textBox2;
+    }
+}
---
+++ CountryFromIP/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Web;
+using System.Net;
+
+namespace CountryFromIP
+{
+    public partial class Form1 : Form
+    {
+        private const string API = "http://ip-api.com/csv/";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.textBox2.Text = getCountry(textBox1.Text);
+        }
+
+        private string getCountry(string ip)
+        {
+            string response = new WebClient().DownloadString(API + ip);
+            return response.StartsWith("fail,") ? "N/A" : response.Split(',')[1].Replace("\"","");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
